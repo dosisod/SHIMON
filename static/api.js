35edf4937419b0fe06560b9e872082fa178df5c4
dosisod <39638017+dosisod@users.noreply.github.com>
2019-04-11 22:46:51 -0700

    post can now scrape or follow

--- static/api.js
+++ static/api.js
@@ -1,20 +1,30 @@
-function post(arr) { //construct api call from dictionary
-	var form=nu("form", { //make nu empty form
-		"action": "/api/",
-		"method": "POST"
-	})
-
-	for (i in arr) {
-		nu("input", { //for each element make a nu hidden feild
-			"type": "hidden",
-			"name": i,
-			"value": arr[i]
-		}, form)
-	}
-	var submit=nu("input", { //make nu submit button
-		"type": "submit",
-		"style": "visibility: hidden;"
-	}, [form, document.body], true)
+async function post(arr, redirect) { //construct api call from dictionary
+	if (redirect) { //create form, submit and follow it
+		var form=nu("form", { //make nu empty form
+			"action": "/api/",
+			"method": "POST"
+		})
 	
-	submit.click()
+		for (i in arr) {
+			nu("input", { //for each element make a nu hidden feild
+				"type": "hidden",
+				"name": i,
+				"value": arr[i]
+			}, form)
+		}
+		var submit=nu("input", { //make nu submit button
+			"type": "submit",
+			"style": "visibility: hidden;"
+		}, [form, document.body], true)
+		
+		submit.click() //send form
+	}
+	else { //only grab data from api
+		var fd=new FormData()
+		for (i in arr) fd.append(i, arr[i]) //fill formdata
+	
+		return fetch("/api/", {method:"post", body:fd})
+			.then(e=>e.json())
+			.then(e=>{return e}) //return data
+	}
 }
