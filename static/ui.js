935c4115de771a2dab9421a02b5a610437b898a7
dosisod <39638017+dosisod@users.noreply.github.com>
2019-04-12 23:40:37 -0700

    added uuid to img converter

--- static/ui.js
+++ static/ui.js
@@ -1,7 +1,7 @@
+var tray=document.getElementById("tray")
+
 function new_card(u, n, m) { //adds card to tray
 	//uuid, name, message
-
-	tray=document.getElementById("tray")
 
 	var ol=nu("ol")
 	ol.appendChild(
@@ -36,5 +36,21 @@ function new_card(u, n, m) { //adds card to tray
 	tray.appendChild(card)
 }
 function new_img(uuid) { //converts uuid to b64 img of hash
-	//
+	var canv=nu("canvas", {
+		"width": 16,
+		"height": 16
+	})
+	var draw=canv.getContext("2d")
+
+	bin=""
+	for (i of uuid) bin+=parseInt(i,16).toString(2) //hex to binary string)
+	bin="0".repeat(256-bin.length)+bin //add leading bits
+
+	for (i=0;i<256;i++) { //16*16=256 pixels
+		//set black or white pixel
+		draw.fillStyle=(bin[i]=="1")?"rgb(255,255,255)":"rgb(0,0,0)"
+		draw.fillRect(i%16, ~~(i/16), 1, 1) //draw single pixel
+	}
+
+	return canv.toDataURL()
 }
