631664189b3a04794283a3b2d5e1ba0e0f3f1dc0
dosisod <39638017+dosisod@users.noreply.github.com>
2019-04-17 19:05:59 -0700

    post can now take json objects for params

--- static/api.js
+++ static/api.js
@@ -1,4 +1,16 @@
 async function post(arr, redirect) { //construct api call from dictionary
+	var encode=(s)=>{ //if any param passed is an object, jsonify it
+		if (typeof s=="object") {
+			try {
+				return JSON.stringify(s)
+			}
+			catch {
+				return s
+			}
+		}
+		return s
+	}
+
 	if (redirect) { //create form, submit and follow it
 		var form=nu("form", { //make nu empty form
 			"action": "/api/",
@@ -9,7 +21,7 @@ async function post(arr, redirect) { //construct api call from dictionary
 			nu("input", { //for each element make a nu hidden feild
 				"type": "hidden",
 				"name": i,
-				"value": arr[i]
+				"value": encode(arr[i])
 			}, form)
 		}
 		var submit=nu("input", { //make nu submit button
@@ -21,7 +33,7 @@ async function post(arr, redirect) { //construct api call from dictionary
 	}
 	else { //only grab data from api
 		var fd=new FormData()
-		for (i in arr) fd.append(i, arr[i]) //fill formdata
+		for (i in arr) fd.append(i, encode(arr[i])) //fill formdata
 	
 		return fetch("/api/", {method:"POST", body:fd})
 			.then(e=>e.json())
