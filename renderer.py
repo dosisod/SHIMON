84237643c0728ffc0561ad77471ebdc3c95178ba
dosisod <39638017+dosisod@users.noreply.github.com>
2019-09-20 21:42:31 -0700

    made css files templates and added darkmode capabilities

--- renderer.py
+++ renderer.py
@@ -3,6 +3,10 @@ from flask import render_template
 #when a template is rendered with developer mode on, the .js files will be used
 #when developer mode is off, the shimon.min.js file is used to improve proformance
 
-def render(self, name, **kwargs): #automatically add devmode state to all render_templates
+#when darkmode is on, style sheets will use darker colors and patterns
+#when off (default), normal white theme is used
+
+def render(self, name, **kwargs): #automatically add devmode and darkmode state to all render_templates
 	kwargs["developer"]=self.developer
+	kwargs["darkmode"]=self.darkmode
 	return render_template(name, **kwargs)
