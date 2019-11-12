5a3d16f5232042ed7d37162a06cb3532b6d3fceb
dosisod <39638017+dosisod@users.noreply.github.com>
2019-11-11 21:36:04 -0800

    bumped up version to 0.0.25

--- SHIMON/shimon.py
+++ SHIMON/shimon.py
@@ -20,7 +20,7 @@ from .__init__ import Page, Json
 
 class Shimon:
 	def __init__(self) -> None:
-		self.VERSION="0.0.24"
+		self.VERSION="0.0.25"
 
 		self.cache=None #stores cached data after decryption
 
