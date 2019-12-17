5ff4a11fb903e01c5751d186492654aca3b3c53b
dosisod <39638017+dosisod@users.noreply.github.com>
2019-12-16 21:45:17 -0800

    cleaned up how css is being built out/rendered

--- min.sh
+++ min.sh
@@ -1,5 +1,7 @@
 #clear old js file
 >SHIMON/static/js/shimon.min.js
+>SHIMON/static/css/bundle.css
+>SHIMON/static/css/login.css
 
 if [ -z "$@" ]; then
 	#print files to minify
@@ -21,4 +23,12 @@ if [ -z "$@" ]; then
 
 	#minify all css into a single bundle
 	minify $(tree -fi src/css/ | head -n -2 | grep -F ".css" | grep -v "bundle.css" | grep -v "login.css") > "SHIMON/static/css/bundle.css"
+
+	echo ""
+	echo "minifying:"
+	echo "font.css"
+	echo "login.css"
+
+	minify src/css/font.css > "SHIMON/static/css/login.css"
+	minify src/css/login.css >> "SHIMON/static/css/login.css"
 fi
