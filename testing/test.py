45d299cff7feb1f705027b3ec174b0555d90acec
dosisod <39638017+dosisod@users.noreply.github.com>
2019-12-23 23:13:00 -0800

    changed imports and added new tests

--- testing/test.py
+++ testing/test.py
@@ -2,36 +2,41 @@ from flask import Flask, Response
 from flask.json import jsonify
 import os
 
-from SHIMON.storage import unlock, lock, locker
-from SHIMON.api.handle import handler
-from SHIMON.renderer import render
-from SHIMON.api.error import error
+from SHIMON import storage
+from SHIMON import api
 
 from SHIMON.app import App
 
 class Test:
 	pwd="123"
 
-	app=App()
+	test_app=App()
 
 	def test_storage(self):
-		assert unlock(self.pwd)!="{}"
+		assert storage.unlock(self.pwd)!="{}"
 
 	def test_error(self):
-		with self.app.app.app_context():
+		with self.test_app.app.app_context():
 			#test that True and "true" are treated the same
+			assert api.error.error(200, "test1", True)[0]=="test1"
+
 			#assert strings are returned as-is
-			assert error(200, "test1", True)[0]=="test1"
-			assert error(200, "test2", "true")[0]=="test2"
+			assert api.error.error(200, "test2", "true")[0]=="test2"
 
 			#assert ints are returned as-is
-			assert error(200, 1337, True)[0]==1337
+			assert api.error.error(200, 1337, True)[0]==1337
 
 			#assert lists are jsonified before returning
-			assert error(200, ["test3"], True)[0].json==jsonify(["test3"]).json
+			assert api.error.error(200, ["test3"], True)[0].json==jsonify(["test3"]).json
 
 			#assert that rethrow will return blank rethrow json
-			assert error(200, None, False, True)[0].json==jsonify({"rethrow":""}).json
+			assert api.error.error(200, None, False, True)[0].json==jsonify({"rethrow":""}).json
 
 			#assert that an unreturnable type will be set to "" before sending back
-			assert error(200, None, False)[0].json==jsonify({"code":200,"msg":""}).json
+			assert api.error.error(200, None, False)[0].json==jsonify({"code":200,"msg":""}).json
+
+			#assert default error 200 msg is the same as the explicit call
+			assert api.error.error_200()[0].json==api.error.error(200, "OK", False)[0].json
+
+			#assert custom error 200 msg is the same as the explicit version
+			assert api.error.error_200("test")[0].json==api.error.error(200, "test", False)[0].json
