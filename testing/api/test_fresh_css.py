00650bd48cfd24c1f368a8b7a8efbb658b652d19
dosisod <39638017+dosisod@users.noreply.github.com>
2020-03-04 22:31:50 -0800

    fixed fresh css/js tests

--- testing/api/test_fresh_css.py
+++ testing/api/test_fresh_css.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+import shutil
+import os
+
 from SHIMON.api.fresh_css import fresh_css
 
 from testing.base import BaseTest
 from testing.util import assertHttpResponse
 
 class TestFreshCSS(BaseTest):
+	css_file="SHIMON/static/css/font.css"
+
 	@BaseTest.request_context
 	def test_always_returns_http_200(self):
 		assertHttpResponse(
@@ -12,16 +18,41 @@ class TestFreshCSS(BaseTest):
 		)
 
 	@BaseTest.request_context
-	def test_enabled_when_set_to_true(self):
+	def test_not_having_fresh_css_ignores_value(self):
 		self.shimon.cache.mapper["fresh css"]=False
 
-		self.fresh_css(True)
+		if os.path.isfile(self.css_file):
+			shutil.move(self.css_file, self.css_file + ".bak")
+
+			self.fresh_css(True)
+
+			shutil.move(self.css_file + ".bak", self.css_file)
+
+		else:
+			self.fresh_css(True)
+
+		assert self.shimon.fresh_css==False
+		assert self.shimon.cache["fresh css"]==False
+
+	@BaseTest.request_context
+	def test_having_fresh_css_respects_value(self):
+		self.shimon.cache.mapper["fresh css"]=False
+
+		if not os.path.isfile(self.css_file):
+			Path(self.css_file).touch()
+
+			self.fresh_css(True)
+
+			os.remove(self.css_file)
+
+		else:
+			self.fresh_css(True)
 
 		assert self.shimon.fresh_css==True
 		assert self.shimon.cache["fresh css"]==True
 
 	@BaseTest.request_context
-	def test_disabled_when_set_to_non_true(self):
+	def test_disabled_when_set_to_false(self):
 		self.shimon.cache.mapper["fresh css"]=False
 
 		self.fresh_css(False)
--- testing/api/test_fresh_js.py
+++ testing/api/test_fresh_js.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+import shutil
+import os
+
 from SHIMON.api.fresh_js import fresh_js
 
 from testing.base import BaseTest
 from testing.util import assertHttpResponse
 
 class TestFreshCSS(BaseTest):
+	js_file="SHIMON/static/js/api.js"
+
 	@BaseTest.request_context
 	def test_always_returns_http_200(self):
 		assertHttpResponse(
@@ -12,16 +18,42 @@ class TestFreshCSS(BaseTest):
 		)
 
 	@BaseTest.request_context
-	def test_enabled_when_set_to_true(self):
+	def test_not_having_fresh_js_ignores_value(self):
 		self.shimon.cache.mapper["fresh js"]=False
 
-		self.fresh_js(True)
+		if os.path.isfile(self.js_file):
+			shutil.move(self.js_file, self.js_file + ".bak")
+
+			self.fresh_js(True)
+
+			shutil.move(self.js_file + ".bak", self.js_file)
+
+		else:
+			self.fresh_js(True)
+
+		assert self.shimon.fresh_js==False
+		assert self.shimon.cache["fresh js"]==False
+
+	@BaseTest.request_context
+	def test_having_fresh_js_respects_value(self):
+		self.shimon.cache.mapper["fresh js"]=False
+
+		js_file="SHIMON/static/js/api.js"
+		if not os.path.isfile(js_file):
+			Path(js_file).touch()
+
+			self.fresh_js(True)
+
+			os.remove(js_file)
+
+		else:
+			self.fresh_js(True)
 
 		assert self.shimon.fresh_js==True
 		assert self.shimon.cache["fresh js"]==True
 
 	@BaseTest.request_context
-	def test_disabled_when_set_to_non_true(self):
+	def test_disabled_when_set_to_false(self):
 		self.shimon.cache.mapper["fresh js"]=False
 
 		self.fresh_js(False)
