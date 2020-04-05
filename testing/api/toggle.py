815a0a6214e54ea8560c1d460a7f61e6faba77f7
dosisod <39638017+dosisod@users.noreply.github.com>
2020-04-04 17:31:44 -0700

    fixed tests

--- testing/api/toggle.py
+++ testing/api/toggle.py
@@ -15,45 +15,28 @@ class FreshToggle(BaseTest):
 		pass
 
 	@BaseTest.request_context
-	def test_not_having_fresh_file_always_returns_http_400(self) -> None:
-		assertHttpResponse(
-			self.func(True),
-			200
-		)
-
-		assertHttpResponse(
-			self.func(False),
-			200
-		)
-
-	@BaseTest.request_context
 	def test_not_having_fresh_file_ignores_value(self) -> None:
 		self.run_without_fresh_file()
-
 		self.assertValue(False)
 
 	@BaseTest.request_context
-	def test_not_having_fresh_file_returns_error(self) -> None:
+	def test_enabling_without_having_fresh_file_returns_http_400(self) -> None:
 		assertHttpResponse(
 			self.run_without_fresh_file(),
 			400
 		)
 
 	@BaseTest.request_context
-	def test_having_fresh_file_respects_value(self) -> None:
-		self.shimon.cache.mapper[self.name]=False
-
-		if not os.path.isfile(self.path):
-			Path(self.path).touch()
-
-			self.func(True)
-
-			os.remove(self.path)
-
-		else:
-			self.func(True)
-
+	def test_having_fresh_file_accepts_value(self) -> None:
+		self.run_with_fresh_file()
 		self.assertValue(True)
+
+	@BaseTest.request_context
+	def test_enabling_with_fresh_file_returns_http_200(self) -> None:
+		assertHttpResponse(
+			self.run_with_fresh_file(),
+			200
+		)
 
 	@BaseTest.request_context
 	def test_disabled_when_set_to_false(self) -> None:
@@ -77,6 +60,19 @@ class FreshToggle(BaseTest):
 			shutil.move(self.path, self.path + ".bak")
 			ret=self.func(True)
 			shutil.move(self.path + ".bak", self.path)
+
+		else:
+			ret=self.func(True)
+
+		return ret
+
+	def run_with_fresh_file(self) -> HttpResponse:
+		self.shimon.cache.mapper[self.name]=False
+
+		if not os.path.isfile(self.path):
+			Path(self.path).touch()
+			ret=self.func(True)
+			os.remove(self.path)
 
 		else:
 			ret=self.func(True)
