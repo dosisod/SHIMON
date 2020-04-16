06e5ccbddcf7ae90d338cd48b2720df22e4704b1
dosisod <39638017+dosisod@users.noreply.github.com>
2020-04-16 14:34:32 -0700

    moved util.ts into url.ts

--- src/ts/url.ts
+++ src/ts/url.ts
@@ -1,3 +1,10 @@
 function url(path: string): void {
 	history.replaceState({}, "", "/" + path)
 }
+
+function cookie(name: string) {
+	return document.cookie.replace(
+		new RegExp("(?:(?:^|.*;\\s*)" + name + "\\s*\\=\\s*([^;]*).*$)|^.*$"),
+		"$1"
+	)
+}
--- src/ts/util.ts
+++
@@ -1,6 +0,0 @@
-function cookie(name: string) {
-	return document.cookie.replace(
-		new RegExp("(?:(?:^|.*;\\s*)" + name + "\\s*\\=\\s*([^;]*).*$)|^.*$"),
-		"$1"
-	)
-}
