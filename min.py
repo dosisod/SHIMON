b41337c62d139638780cddff2ca1e37b9f10f245
dosisod <39638017+dosisod@users.noreply.github.com>
2020-05-05 16:42:49 -0700

    cleaned up minifier

--- min.py
+++ min.py
@@ -1,32 +1,34 @@
 from pathlib import Path
 import subprocess
-import shutil
 import sys
+import re
 
-from typing import List, Iterator, Union, Optional
+from typing import List, Iterator
 
-def clear(filename: Union[str, List[str]]) -> None:
-	if isinstance(filename, list):
-		for fn in filename:
-			clear(fn)
+def clear(file: str) -> None:
+	with open(file, "w+") as f:
+		f.write("")
 
-	else:
-		with open(filename, "w+") as f:
-			f.write("")
+def should_ignore(path: str, ignore: List=[]) -> bool:
+	for check in ignore:
+		if re.match(check, path):
+			return True
 
-def files(path: str, ignore: List=[], fullpath: bool=False) -> Iterator[str]:
-	path=Path(path)
+	return False
 
-	for filename in path.iterdir():
-		if filename.name not in ignore and not filename.name.endswith(".swp"):
-			yield str(filename.resolve()) if fullpath else str(filename.name)
+def files(path: str, ignore: List=[]) -> Iterator[str]:
+	ignore.append(r".*\.swp")
+
+	for file in Path(path).iterdir():
+		if not should_ignore(file.name, ignore):
+			yield file
 
 def sass(src: str, dest: str="") -> bytes:
 	return call([
 		"sass",
 		"--no-source-map",
 		"-s", "compressed",
-		(f"{src}:{dest}") if dest else src
+		f"{src}:{dest}" if dest else src
 	])
 
 def call(commands: List[str]) -> bytes:
@@ -35,53 +37,45 @@ def call(commands: List[str]) -> bytes:
 		stdout=subprocess.PIPE
 	).stdout
 
-STATIC_CSS="SHIMON/static/css/"
-STATIC_JS="SHIMON/static/js/"
-THEMES="SHIMON/templates/themes/"
+STATIC_CSS=Path("SHIMON/static/css/")
+STATIC_JS=Path("SHIMON/static/js/")
+THEMES=Path("SHIMON/templates/themes/")
 
-BUNDLE_JS=STATIC_JS + "bundle.js"
-BUNDLE_CSS=STATIC_CSS + "bundle.css"
+BUNDLE_CSS=STATIC_CSS / "bundle.css"
+BUNDLE_JS=STATIC_JS / "bundle.js"
 
-#force clear given files
-clear([
-	BUNDLE_JS,
-	BUNDLE_CSS,
-	STATIC_CSS + "login.css"
-])
+[clear(f) for f in [BUNDLE_JS, BUNDLE_CSS, STATIC_CSS / "login.css"]]
 
 skipping=len(sys.argv) > 1
 
 if not skipping:
 	print("minifying JS")
 
-	for filename in files(STATIC_JS, ignore=["bundle.js"]):
-		print(f"  {filename}")
+	for file in files(STATIC_JS, ignore=["bundle.js"]):
+		print(f"  {file.name}")
 
 	with open(BUNDLE_JS, "rb+") as f:
 		f.write(
-			call(["minify"] + list(
-				files(
-					STATIC_JS,
-					["bundle.js"],
-					fullpath=True
-				)
-			))
+			call(["minify", *files(
+				STATIC_JS,
+				["bundle.js"]
+			)])
 		)
 
 print("\ncopying CSS files")
-for filename in files("src/css/"):
+for file in files("src/css/"):
 	sass(
-		f"src/css/{filename}",
-		STATIC_CSS + filename[:-4] + "css"
+		f"src/css/{file.name}",
+		STATIC_CSS / file.name.replace("scss", "css")
 	)
 
 if not skipping:
 	print("\nminifying CSS")
 
 	with open(BUNDLE_CSS, "rb+") as f:
-		for filename in files("src/css/", ignore=["bundle.css", "login.scss", "_screen.scss"], fullpath=True):
-			print("  " + filename.split("/")[-1])
-			f.write(sass(filename))
+		for file in files("src/css/", ignore=["bundle.css", "login.scss", "_screen.scss"]):
+			print(f"  {file.name}")
+			f.write(sass(file))
 
 	print("\nminifying")
 	print("  font.css")
@@ -90,7 +84,7 @@ if not skipping:
 	# minify and combine these 2 css files together
 	# they are not needed in the bundle, only the login
 
-	with open(STATIC_CSS + "login.css", "wb+") as f:
+	with open(STATIC_CSS / "login.css", "wb+") as f:
 		f.write(
 			sass("src/css/font.scss") +
 			sass("src/css/login.scss")
@@ -98,30 +92,25 @@ if not skipping:
 
 	print("\nminifying themes:")
 
-	for filename in files("src/themes", ignore=["_urls.scss", "_root.scss", "_theme.scss"]):
-		css_name=filename[:-4] + "css"
+	for file in files("src/themes", ignore=[r"^_.*\.scss"]):
+		css_name=file.name.replace("scss", "css")
 
-		print(f"  {filename}")
-		clear(THEMES + css_name)
+		print(f"  {file.name}")
+		clear(THEMES / css_name)
 
 		sass(
-			"src/themes/" + filename,
-			THEMES + css_name
+			"src/themes/" + file.name,
+			THEMES / css_name
 		)
 
-	# prepend license for solarized colorscheme
-
 	solarized_license= \
-"""/* Reference to the original colorscheme */
-/* github.com/altercation/solarized      */
-
+b"""/* Reference to the original colorscheme */
+/* github.com/altercation/solarized      */\n
 """
 
-	css=b""
-	with open(THEMES + "solarized dark.css", "rb") as f:
+	with open(THEMES / "solarized dark.css", "rb+") as f:
 		css=f.read()
-
-	with open(THEMES + "solarized dark.css", "wb") as f:
-		f.write(solarized_license.encode() + css)
+		f.seek(0)
+		f.write(solarized_license + css)
 
 print("")
